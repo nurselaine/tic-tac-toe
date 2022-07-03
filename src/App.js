35ef438668001ec07953db0a3e075922a29c1da4
nurselaine <Elaine.t.huynh@gmail.com>
2022-07-02 17:02:53 -0700

    adding react app files

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import Game from './components/Game';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Tic-Tac-Toe</h2>
+      <Game/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Square.js
@@ -0,0 +1,12 @@
+function Square(props) {
+    return (
+      <button 
+        onClick={props.onClick} 
+        className="square"
+      >
+        {props.value}
+      </button>
+    );
+}
+
+export default Square;
